047545d93fdc3964b5b1627eb279400e1560cd4a
audreyarbuckle@icloud.com <audreyarbuckle@icloud.com>
2025-10-09 00:01:30 -0700

    unfortunatly not done but what i could do since the issue i had at 5

--- generic-api-audrey/src/App.jsx
+++ generic-api-audrey/src/App.jsx
@@ -3,26 +3,99 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import StarWarsLogo from './starwars.png';
+import planet1 from './hoth.png'
+import planet2 from './naboo.png'
+import planet3 from './coruscant.png'
+import planet4 from './tatooine.png'
+
+console.log(StarWarsLogo);
+console.log(planet1);
+console.log(planet2);
+console.log(planet3);
+console.log(planet4);
+
 function App() {
   const [count, setCount] = useState(0)
+  const [visible, setVisible] = useState(false); 
+  const [planets, setPlanets] = useState([]);
+  const fetchPlanets = async () => {
+    const response = await fetch("https://swapi.dev/api/planets/");
+    const data = await response.json();
+    const chosenIndexes = [3, 7, 8, 0];
+    const selectedPlanets = chosenIndexes.map(i => data.results[i]);
+    setPlanets(selectedPlanets);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={StarWarsLogo} class = 'title' alt="logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Planets</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <div class = "container button">
+            <img src={planet1} class = 'logo' alt="logo" onClick={() => setVisible(true) }/>
+            {planets.map((planet, index) => (
+            <div key={index} className="card">
+            <button style={{ visibility: visible ? "visible" : "hidden" }} onClick={fetchPlanets}>
+              <h2>{planet.name}</h2>
+              <p><strong>Climate:</strong> {planet.climate}</p>
+              <p><strong>Terrain:</strong> {planet.terrain}</p>
+              <p><strong>Population:</strong> {planet.population}</p> 
+            </button>
+            </div>
+            ))}
+          </div>
+
+
+          <div class = "container button">
+            <img src={planet2} class = 'logo' alt="logo" onClick={() => setVisible(true) }/>
+            {planets.map((planet, index) => (
+            <div key={index} className="card">
+            <button style={{ visibility: visible ? "visible" : "hidden" }} onClick={fetchPlanets}>
+              <h2>{planet.name}</h2>
+              <p><strong>Climate:</strong> {planet.climate}</p>
+              <p><strong>Terrain:</strong> {planet.terrain}</p>
+              <p><strong>Population:</strong> {planet.population}</p> 
+            </button>
+            </div>
+            ))}
+          </div>
+
+
+          <div class = "container button">
+            <img src={planet3} class = 'logo' alt="logo" onClick={() => setVisible(true) }/>
+            {planets.map((planet, index) => (
+            <div key={index} className="card">
+            <button style={{ visibility: visible ? "visible" : "hidden" }} onClick={fetchPlanets}>
+              <h2>{planet.name}</h2>
+              <p><strong>Climate:</strong> {planet.climate}</p>
+              <p><strong>Terrain:</strong> {planet.terrain}</p>
+              <p><strong>Population:</strong> {planet.population}</p> 
+            </button>
+            </div>
+            ))}
+          </div>
+
+
+          <div class = "container button">
+            <img src={planet4} class = 'logo' alt="logo" onClick={() => setVisible(true) }/>
+            {planets.map((planet, index) => (
+            <div key={index} className="card">
+            <button style={{ visibility: visible ? "visible" : "hidden" }} onClick={fetchPlanets}>
+              <h2>{planet.name}</h2>
+              <p><strong>Climate:</strong> {planet.climate}</p>
+              <p><strong>Terrain:</strong> {planet.terrain}</p>
+              <p><strong>Population:</strong> {planet.population}</p> 
+            </button>
+            </div>
+            ))}
+          </div>
+          
+          
         </p>
       </div>
       <p className="read-the-docs">
